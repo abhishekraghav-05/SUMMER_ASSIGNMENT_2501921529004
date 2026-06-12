890bab1c188e83407e9f53cf60f5e24346083063
abhishekraghav-05 <abhishek.raghav89200@gmail.com>
2026-06-13 00:15:21 +0530

    Add files via upload

---
+++ WEEK_2/OOPS/music/Test.java
@@ -0,0 +1,14 @@
+package music;
+
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args) {
+        Veena v = new Veena();
+        v.play();
+
+        Saxophone s = new Saxophone();
+        s.play();
+    }
+}
